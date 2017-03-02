ad32a2112310f80e8d962bda16d6bf076f1845e7
Pedro Brighenti <pedro.brighenti@gmail.com>
2017-03-02 12:57:39 +0000

    implemented damage scaling and custom monster creation. Missing attack method

---
+++ networkingProject/src/org/academiadecodigo/roothless/server/gameObjects/Attacker.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.roothless.server.gameObjects;
+
+/**
+ * Created by codecadet on 02/03/17.
+ */
+public interface Attacker {
+}
---
+++ networkingProject/src/org/academiadecodigo/roothless/server/gameObjects/monsters/TestMonster.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.roothless.server.gameObjects.monsters;
+
+/**
+ * Created by codecadet on 02/03/17.
+ */
+public class TestMonster {
+}
