fa47d2aa717b1d44bdf97e2e03b73f6de7d5e8d8
Luís Blümel <luis.blumel@gmail.com>
2017-03-02 23:52:31 +0000

    game will start only if the 5 players are online and then it'll give the intro to the Stage, so that it'll start the first room

--- networkingProject/src/org/academiadecodigo/roothless/server/Game.java
+++ networkingProject/src/org/academiadecodigo/roothless/server/Game.java
@@ -5,12 +5,54 @@ package org.academiadecodigo.roothless.server;
  */
 public class Game {
 
+    private Stage stage;
+    private boolean gameOn;         //if False when game is over and when the 5 players aren't on yet
+    private boolean hasNewMessage;  //to check if there's a new message to deliver to the Server
+
+    public Game(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Game(){
+        stage = new Stage();
+    }
+
+    public boolean isGameOn() {                         // TODO: 02/03/17  Server must have a while(true) { if(!isGameOn() && isListFull()) { break; } }
+        return gameOn;
+    }
+
+    public boolean canIStart(boolean isListFull) {      // TODO: 01/03/17 Server must have a while(true){  if(game.canIStart(isListFull)){ break; }  }
+        if (isListFull) {
+            gameOn = true;
+            start();
+            return true;
+        }
+        return false;
+    }
 
 
+    public String start() {
 
-    public void start(){
+        String intro = "You are part of a group of 5 brave adventurers that received a special mission from the most " +
+                "iconic Emperor that history has ever seen, his highness Trumpet Piglet, the brand new ruler of the " +
+                "Ultra Safe and Armed Kingdoms (USAK - please don't you ever read it as \"You Sack\" or \"You Suck\", " +
+                "or else your head will roll - it's a nation of defenders of the Truth, Justice and Freedom, ok? " +
+                "Difamation is not freedom of speech, and \"inconvenient truths\" are just alternative facts). " +
+                "Anyway, the Emperor told you that he has a huuuge but very simple and effective plan " +
+                "to make the empire as great as it used to be.\nYou're a bit confused because you can't really recall such times, " +
+                "but anyway, to make that plan come true, you must contribute by completing your mission." +
+                "\nBasically there's this evil guild called DAESH (Dangerous Armed Enemies with Strange Habits). " +
+                "Your mission is to penetrate their HQ and just obliterate them!\n\n[You can chat with each other at anytime, " +
+                "but for executing commands type \"/[name of command]\". Try typing \"help\" for the full list of commands].";
+
+        stage.enterdungeon(intro);
+
+
+        //// TODO: 02/03/17 For the stage to start the first room (enter Dungeon), it will first receive this intro, broadcast it and then start the first room.
 
     }
+
+
 
 
 }
